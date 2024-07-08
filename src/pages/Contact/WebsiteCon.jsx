a82e49d3b23c8f607221b9bcc9d9c60b0ce9927e
Daviwizzy <davidakintunde433@gmail.com>
2024-07-08 13:55:04 +0100

    Initial commit

--- src/pages/Contact/WebsiteCon.jsx
+++ src/pages/Contact/WebsiteCon.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import Header from '../../components/Header/Header';
-import Footer from '../../components/Footer/Footer';
 import ContactForm from './Contact';
 
 const WebsiteCon = () => {
   return (
     <div className="white-gradient-left" style={{ backgroundColor: '#f0f0f0' }}>
-      
       <ContactForm />
-    
     </div>
   );
 }
